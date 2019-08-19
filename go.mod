ab73f721390b0a64c62f9af760b5db0fef701e63
Zhan9Yunhua <zy.hua1122@qq.com>
2019-08-19 23:14:44 +0800

    u

--- go.mod
+++ go.mod
@@ -9,13 +9,13 @@ require (
 	github.com/drhodes/golorem v0.0.0-20160418191928-ecccc744c2d9
 	github.com/gin-gonic/gin v1.4.0
 	github.com/go-kit/kit v0.9.0
-	github.com/go-logfmt/logfmt v0.4.0 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/gogo/protobuf v1.2.1 // indirect
 	github.com/golang/protobuf v1.3.2
 	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/gorilla/handlers v1.4.2
 	github.com/gorilla/mux v1.7.3
+	github.com/prometheus/client_golang v1.1.0 // indirect
 	go.etcd.io/etcd v3.3.13+incompatible
 	google.golang.org/grpc v1.22.1 // indirect
 	gopkg.in/yaml.v2 v2.2.2
--- servers/user/cmd/main.go
+++ servers/user/cmd/main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
-	"github.com/Zhan9Yunhua/blog-svr/servers/user/etcd"
+	"net/http"
+
 	_ "github.com/Zhan9Yunhua/blog-svr/servers/user/config"
+	"github.com/Zhan9Yunhua/blog-svr/servers/user/etcd"
 	"github.com/Zhan9Yunhua/blog-svr/servers/user/logger"
+	"github.com/Zhan9Yunhua/blog-svr/servers/user/middleware"
+	"github.com/Zhan9Yunhua/blog-svr/servers/user/service"
 )
 
 func main() {
 	lg := logger.NewLogger()
 
-
 	etcdClient := etcd.NewEtcd()
 
-	register:=etcd.Register(etcdClient,lg)
+	register := etcd.Register(etcdClient, lg)
 	defer register.Deregister()
 
+	var ucenterSvc service.UcenterServiceInterface
+	ucenterSvc = service.UcenterService{}
+	ucenterSvc = middleware.InstrumentingMiddleware()(ucenterSvc)
 
+	mux := http.NewServeMux()
+	mux.Handle("/svc/ucenter/v1/", service.MakeHandler(ucenterSvc, httpLogger))
 }
